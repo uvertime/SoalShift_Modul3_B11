6cae9b24d9f0d1ce5da0043619988051b38bd186
uvertime <chrisnady99@gmail.com>
2017-10-25 16:34:52 +0700

    function fact

--- soal4.c
+++ soal4.c
@@ -3,6 +3,20 @@
 #include <stdlib.h>
 #include <unistd>
 
+void *fact(void *arg) {
+        int n; 
+        n = *((int *) arg);
+
+        int i;
+        long double result = 1;
+        for (i = 2; i <= n; i++) {  //loop factorial
+                result =result*i;
+        }
+
+        
+
+}
+
 
 int main(int argc, char *argv[]) {
 	pthread_t tid[argc-1];
