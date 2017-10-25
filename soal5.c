ec84ebf115afa17f0727eae65c79daba0f48a8b9
nizarbecak <32842771+nizarbecak@users.noreply.github.com>
2017-10-25 16:59:39 +0700

    Add files via upload
    mboh cak

---
+++ soal5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+id[100];//inisialisasi array untuk menampung thread dalam kasusu ini ada 2 thread
+int status = 0;
+
